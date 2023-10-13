e0867854dcd460bc871c6421bf4d2ec25e2e36bd
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-10-13 09:10:28 +0200

    pblm affichage

--- classes/Livre.php
+++ classes/Livre.php
@@ -84,7 +84,7 @@ class Livre {
     //    return $result;
     //}
 
-    public function __toString() {  
+    public function __toString() {   
         return $this->titre." ".$this->dateParution->format("Y")." ".$this->nbPages." ".$this->prix;
     }
 }
